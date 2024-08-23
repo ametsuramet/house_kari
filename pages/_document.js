235fa3a8111f5bd56fd4e8e3367e7f59bf8ad134
flyyy26 <rfchannel0@gmail.com>
2024-08-23 14:10:16 +0700

    Analytic

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -13,10 +12,23 @@ class MyDocument extends Document {
           <link rel="alternate" hrefLang="zh" href="/zh" />
           <link rel="alternate" hrefLang="x-default" href="/" />
           <link rel="icon" href="/favicon.png" />
-          <Script id="tracking-api-key" strategy="beforeInteractive">
-            {`window.trackingApiKey = 'mhHoCDQEPiYD7vU37K5AX0bKuP86a31wU2P8N86L';`}
-          </Script>
-          <Script src="https://prahwa.net/tracking.js" strategy="beforeInteractive" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.trackingApiKey = 'mhHoCDQEPiYD7vU37K5AX0bKuP86a31wU2P8N86L';
+              `,
+            }}
+          />
+          <script
+            src="https://prahwa.net/tracking.js"
+            strategy="afterInteractive" // Atur strategi pemuatan skrip
+            onLoad={() => {
+              console.log('Tracking script loaded successfully.');
+            }}
+            onError={(e) => {
+              console.error('Error loading tracking script:', e);
+            }}
+          />
         </Head>
         <body>
           <Main />
